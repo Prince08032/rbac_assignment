9e5bbdc85cbf0b1ea0fc8a8907c8da08d3d4f74c
Prince Raj <princer6450@gmail.com>
2024-11-24 23:38:44 +0530

    Update

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://princer6450:VGJr9ZluNukjdtpR@cluster0.q2ssi.mongodb.net/rbac_db?retryWrites=true&w=majority&appName=Cluster0';
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
